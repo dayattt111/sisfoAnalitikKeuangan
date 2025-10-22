2eb0615726cbe7664441b5fcac35799557ee266b
dayattt111 <awdayaa1@gmail.com>
2025-10-23 01:23:31 +0800

    feat: logins

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,33 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Route::get('/admin/dashboard', function () {
+    //     return view('dashboard', ['role' => 'admin']);
+    // })->name('admin.dashboard')->middleware('role.admin');
+    // Route::get('/manager/dashboard', function () {
+    //     return view('dashboard', ['role' => 'manager']);
+    // })->name('manager.dashboard')->middleware('role.manager');
+    // Route::get('/staff/dashboard', function () {
+    //     return view('dashboard', ['role' => 'staff']);
+    // })->name('staff.dashboard')->middleware('role.staff');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('dashboard', ['role' => 'Admin']);
+    })->name('admin.dashboard')->middleware('role:admin');
+
+    Route::get('/manager/dashboard', function () {
+        return view('dashboard', ['role' => 'Manager']);
+    })->name('manager.dashboard')->middleware('role:manager');
+
+    Route::get('/staff/dashboard', function () {
+        return view('dashboard', ['role' => 'Staff']);
+    })->name('staff.dashboard')->middleware('role:staff');
+});    
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
